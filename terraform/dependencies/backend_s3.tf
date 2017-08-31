2b9553e1222b6a27fb2a96aa0032d6910b90c1ad
yros aguiar <yrosaguiar@gmail.com>
2017-08-31 02:17:42 -0300

    Add ignore changes backend bucket

--- terraform/dependencies/backend_s3.tf
+++ terraform/dependencies/backend_s3.tf
@@ -7,6 +7,10 @@ resource "aws_s3_bucket" "sprinklr_terraform" {
   bucket = "sprinklr-test-terraform-states"
   acl    = "private"
 
+  lifecycle {
+        ignore_changes = ["bucket"]
+  }
+
   tags {
     Name        = "sprinklr-test-terraform-states"
     Environment = "production"
